73ea481680c9bf7f08f4d210fac519ca483e2211
Sarthak1306 <sarthak13062001@gmail.com>
2023-07-21 00:47:18 +0530

    Init

---
+++ model/main.py
@@ -0,0 +1,15 @@
+import pandas as pd  # type: ignore
+
+
+def get_clean_data():
+    data = pd.read_csv("data/data.csv")
+    print(data.head())
+    return data
+
+
+def main():
+    data = get_clean_data()
+
+
+if __name__ == "__main__":
+    main()
