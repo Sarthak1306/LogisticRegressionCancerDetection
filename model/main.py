989e96a2b050a93d20f9be513121d33e5dea7958
Sarthak1306 <sarthak13062001@gmail.com>
2023-07-21 01:05:01 +0530

    data cleaning

--- model/main.py
+++ model/main.py
@@ -3,7 +3,11 @@ import pandas as pd  # type: ignore
 
 def get_clean_data():
     data = pd.read_csv("data/data.csv")
-    print(data.head())
+
+    data = data.drop(["Unnamed: 32", "id"], axis=1)
+
+    data["diagnosis"] = data["diagnosis"].map({"M": 1, "B": 0})
+
     return data
 
 
