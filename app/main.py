26307ec77a17849521ce419b8b050f4259983ebe
Sarthak1306 <sarthak13062001@gmail.com>
2023-07-21 13:52:00 +0530

    st app page layout

--- app/main.py
+++ app/main.py
@@ -11,7 +11,19 @@ def main():
         initial_sidebar_state="expanded",
     )
 
-    st.write("Hello World")
+    with st.container():
+        st.title("Breast Cancer Predictor")
+        st.write(
+            "Connect this app to your cytology lab to help diagnose breast cancer form your tissue sample. This app predicts using a machine learning model whether a breast mass is benign or malignant based on the measurements it receives from your cytosis lab. You can also update the measurements by hand using the sliders in the sidebar."
+        )
+
+    col1, col2 = st.columns([4, 1])
+
+    with col1:
+        st.write("This is Column 1")
+
+    with col2:
+        st.write("This is Column 2")
 
 
 if __name__ == "__main__":
