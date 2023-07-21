89cbb4812c41cdacd05a544676d924e43bf7cb0f
Sarthak1306 <sarthak13062001@gmail.com>
2023-07-21 13:21:15 +0530

    Training

--- model/main.py
+++ model/main.py
@@ -1,4 +1,32 @@
 import pandas as pd  # type: ignore
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn
+
+
+def create_model(data):
+    X = data.drop(["diagnosis"], axis=1)
+    y = data["diagnosis"]
+
+    # scale the data
+    scaler = StandardScaler()
+    X = scaler.fit_transform(X)
+
+    # split the data
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
+
+    # train
+    model = LogisticRegression()
+    model.fit(X_train, y_train)
+
+    return model, scaler
+
+
+def test_model(model):
+
 
 
 def get_clean_data():
@@ -13,6 +41,9 @@ def get_clean_data():
 
 def main():
     data = get_clean_data()
+    model, scaler = create_model(data)
+
+    test_model(model)
 
 
 if __name__ == "__main__":
