2569e50cfed113ab3aa957aeb628b0b185d9cc6d
Sarthak1306 <sarthak13062001@gmail.com>
2023-07-21 13:43:37 +0530

    create streamlit app

---
+++ app/main.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pickle as pickle
+import pandas as pd
+
+
+def main():
+    st.set_page_config(
+        page_title="Breast Cancer Predictor",
+        page_icon=":female-doctor:",
+        layout="wide",
+        initial_sidebar_state="expanded",
+    )
+
+    st.write("Hello World")
+
+
+if __name__ == "__main__":
+    main()
